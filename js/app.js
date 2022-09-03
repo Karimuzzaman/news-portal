b460aa228cf5f8104d6e4ea0d41370772831824a
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-09-03 19:19:24 +0600

    news card added

--- js/app.js
+++ js/app.js
@@ -1,32 +1,107 @@
 function loadCategory() {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayCategory(data.data.news_category))
+    try {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayCategory(data.data.news_category))
+    }
+    catch (error) {
+        console.log(error);
+
+    }
 }
 
 const displayCategory = categories => {
-    const newsCategory = document.getElementById('news-category');
-    categories.forEach(category => {
-        console.log(category);
-        const divNewsCategory = document.createElement('div');
-        divNewsCategory.classList.add('d-sm-inline');
-        divNewsCategory.classList.add('container');
-        divNewsCategory.classList.add('ms-5');
-        divNewsCategory.innerHTML = `
-        <button class="border border-0" onclick="categoryNews('${category.category_id}')">${category.category_name}</button>
+    try {
+        const newsCategory = document.getElementById('news-category');
+        categories.forEach(category => {
+            // console.log(category);
+            const divNewsCategory = document.createElement('div');
+            divNewsCategory.classList.add('d-sm-inline');
+            divNewsCategory.classList.add('container');
+            divNewsCategory.classList.add('ms-5');
+            divNewsCategory.innerHTML = `
+        <button class="border border-0" onclick="loadCategoryNews('${category.category_id}')">${category.category_name}</button>
     `;
-        newsCategory.appendChild(divNewsCategory);
-    })
+            newsCategory.appendChild(divNewsCategory);
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
-const categoryNews = id => {
-    console.log(id);
+const loadCategoryNews = id => {
+    // console.log(id);
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-    console.log(url);
+    // console.log(url);
+    try {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayCategoryNews(data.data))
 
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
+const displayCategoryNews = datas => {
+
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent = '';
+    datas.forEach(data => {
+        // console.log(data);
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+          <div class="card h-100">
+          <img src="${data.thumbnail_url}" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">${data.title}</h5>
+          <p class="card-text text-truncate">${data.details}</p>
+          <h5 class="card-title">Author Name: ${data.author.name}</h5>
+          <img src="${data.author.img}" style="width: 40px; hight: 40px">
+          <p class="card-text text-truncate">Total View: ${data.total_view}</p>
+          <button onclick = "loadModal('${data._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+         </div>
+    `;
+        newsContainer.appendChild(newsDiv);
+    })
+};
+const loadModal = news_id => {
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    // console.log(url);
+    try {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayModal(data.data[0]))
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+const displayModal = data => {
+
+    console.log(data);
+    const modalTitle = document.getElementById('newsDetailModalLabel');
+    modalTitle.innerText = data.title;
+    const newsDetails = document.getElementById('news-details');
+    newsDetails.innerHTML = `
+        <h5>${data.title}</h5>
+          <p>${data.details}</p>
+          <img src="${data.thumbnail_url}" class="card-img-top" alt="...">
+          <h5 class="card-title">Author Name: ${data.author.name}</h5>
+          <img src="${data.author.img}" style="width: 40px; hight: 40px">
+          <p class="card-text text-truncate">Total View: ${data.total_view}</p>
+          <p class="card-text text-truncate">Publish Date: ${data.author.published_date}</p>
+          <p class="card-text text-truncate">Rating: ${data.rating
+            .number}</p>
+
+    `;
+
+}
 
 loadCategory();
